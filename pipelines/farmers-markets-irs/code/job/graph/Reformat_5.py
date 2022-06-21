0bf87f7e38384d14849526109ac0e78a65ed1c78
nimbusaurus@gmail.com <nimbusaurus@gmail.com>
2022-06-21 23:13:31 +0000

    cleanup

--- pipelines/farmers-markets-irs/code/job/graph/Reformat_5.py
+++
@@ -1,8 +0,0 @@
-from pyspark.sql import *
-from pyspark.sql.functions import *
-from pyspark.sql.types import *
-from job.config.ConfigStore import *
-from job.udfs.UDFs import *
-
-def Reformat_5(spark: SparkSession, in0: DataFrame) -> DataFrame:
-    return in0.select(col("zipcode"), col("agi_stub"), col("STATEFIPS"), col("STATE"), col("N1"))
--- pipelines/farmers-markets-irs/code/job/pipeline.py
+++ pipelines/farmers-markets-irs/code/job/pipeline.py
@@ -12,14 +12,8 @@ def pipeline(spark: SparkSession) -> None:
     df_Filter_1 = Filter_1(spark, df_FarmersMarketsSource)
     df_Aggregate_1 = Aggregate_1(spark, df_Reformat_2)
     df_Reformat_3 = Reformat_3(spark, df_Aggregate_1)
-    df_Aggregate_3 = Aggregate_3(spark, df_IrsZipcodesSource)
-    df_Filter_2 = Filter_2(spark, df_Aggregate_3)
-    df_Reformat_7 = Reformat_7(spark, df_Filter_2)
     df_Reformat_1 = Reformat_1(spark, df_Filter_1)
-    df_Reformat_5 = Reformat_5(spark, df_IrsZipcodesSource)
     df_Aggregate_2 = Aggregate_2(spark, df_Reformat_1)
-    df_Deduplicate_1 = Deduplicate_1(spark, df_Reformat_5)
-    df_Reformat_6 = Reformat_6(spark, df_Deduplicate_1)
     df_Reformat_4 = Reformat_4(spark, df_Aggregate_2)
 
 def main():
